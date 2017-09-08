f8cc58869f3cae9bd49984893639da5586138b2b
berryman <osokingauss@gmail.com>
2017-09-08 14:56:49 +0300

    Replaced tabs with whitespaces

--- BaseTasks/BaseTasks/src/FizzBuzz.cpp
+++ BaseTasks/BaseTasks/src/FizzBuzz.cpp
@@ -9,26 +9,26 @@
 
 int main(int argc, char* argv[])
 {
-	const int maxNum = 100;
-	const int fizzNum = 3;
-	const char* fizzStr = "Fizz";
-	const int buzzNum = 5;
-	const char* buzzStr = "Buzz";
-	for (int i = 1; i <= 100; i++)
-	{
-		if (0 == i % fizzNum)
-		{
-			std::cout << fizzStr << std::endl;
-			continue;
-		}
-		else if (0 == i % buzzNum)
-		{
-			std::cout << buzzStr << std::endl;
-			continue;
-		}
+  const int maxNum = 100;
+  const int fizzNum = 3;
+  const char* fizzStr = "Fizz";
+  const int buzzNum = 5;
+  const char* buzzStr = "Buzz";
+  for (int i = 1; i <= 100; i++)
+  {
+    if (0 == i % fizzNum)
+    {
+      std::cout << fizzStr << std::endl;
+      continue;
+    }
+    else if (0 == i % buzzNum)
+    {
+      std::cout << buzzStr << std::endl;
+      continue;
+    }
 
-		std::cout << i << std::endl;
-	}
+    std::cout << i << std::endl;
+  }
 
-	return 0;
+  return 0;
 }
